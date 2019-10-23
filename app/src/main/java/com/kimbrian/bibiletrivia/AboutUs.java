3b3e43ab132f151fa1aa558c0f0fc71a8e52589c
Brian Kimathi <kimbrian400@gmail.com>
2019-10-23 14:26:00 +0300

    Version 1.0.0

--- app/src/main/java/com/kimbrian/bibiletrivia/AboutUs.java
+++ app/src/main/java/com/kimbrian/bibiletrivia/AboutUs.java
@@ -2,13 +2,37 @@ package com.kimbrian.bibiletrivia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutUs extends AppCompatActivity {
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        b = (Button) findViewById(R.id.visit_website);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Used when user trigger on  visit website
+                String url = "https://github.com/kimbrian400/Bible-Trivia";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                Intent chooser = Intent.createChooser(intent, "Open with");
+                startActivity(chooser);
+
+            }
+        });
+
     }
 }
